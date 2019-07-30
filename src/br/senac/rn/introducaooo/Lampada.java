08db5c64a7333e50e9972bd7bffc0373bf2742a5
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-07-30 19:13:42 -0300

    criacao dos atribtos e metodos

---
+++ src/br/senac/rn/introducaooo/Lampada.java
@@ -0,0 +1,16 @@
+package br.senac.rn.introducaooo;
+
+public class Lampada {
+
+    final Integer FREQUECIA = 60;
+    String cor;
+    Integer potencia;
+    Integer tensao;
+    Float comprimento;
+
+    Boolean acende() {};
+
+    Boolean apaga()  {};
+
+
+}
