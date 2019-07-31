a305e29728b808427d56cbc0c59494f271bc63ec
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-07-30 21:01:10 -0300

    criacao dos contrutores

--- src/br/senac/rn/introducaooo/Lampada.java
+++ src/br/senac/rn/introducaooo/Lampada.java
@@ -9,6 +9,18 @@ public class Lampada {
     public Float comprimento;
     private Boolean estado = false; // não pode ser visto de outros pacotes e inicia com falso (desligado)
 
+    public Lampada() {
+    }
+
+    public Lampada(String cor) {
+        this.cor = cor;
+    }
+
+    public Lampada(String cor, Integer potencia) {
+        this.cor = cor;
+        this.potencia = potencia;
+    }
+
     public void acende() { //
         estado = true; // retrona um estado (true)
     }
