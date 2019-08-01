e392af223959bdd8c61cb9280988e77122def612
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-08-01 20:30:37 -0300

    contas

--- src/br/senac/rn/introducaooo/teste/TesteContaCorrente.java
+++ src/br/senac/rn/introducaooo/teste/TesteContaCorrente.java
@@ -1,23 +1,38 @@
 package br.senac.rn.introducaooo.teste;
 
+import br.senac.rn.introducaooo.Conta;
 import br.senac.rn.introducaooo.ContaCorrente;
 
 public class TesteContaCorrente {
+    //polimorfismo só tem em heranç
+    //quando o objeto assume o comportamento de outra classe
 
     public static void main(String[] args) {
 
-        ContaCorrente cc = new ContaCorrente();
+        ContaCorrente contaFlavio = new ContaCorrente();
 
-        cc.setTitular("Flavio Araujo");
-        cc.setAgencia("1234-8");
-        cc.setNumero("4567-7");
-        cc.setTaxa(1.0);
+        contaFlavio.setTitular("Flavio Araujo");
+        contaFlavio.setAgencia("1234-8");
+        contaFlavio.setNumero("4567-7");
+        contaFlavio.setTaxa(1.0);
 
-        cc.deposita(800.0);
+        contaFlavio.deposita(800.0);
 
+        Conta contaCaio = new Conta();
 
-        System.out.println(cc);
+        contaCaio.setTitular("Caio");
+        contaCaio.setNumero("4567-8");
+        contaCaio.setAgencia("1546-7");
 
+        contaCaio.deposita(1000.0);
+
+        System.out.println(contaCaio);;
+        System.out.println(contaFlavio);
+
+        contaCaio.transfere(contaFlavio,100.0);
+
+        System.out.println(contaCaio);
+        System.out.println(contaFlavio);
 
     }
 }
