15b768c73e79e0de24138d9eaae9ca4b4beeee53
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-08-01 19:18:53 -0300

    add o taxa e usando Conta como herança

---
+++ src/br/senac/rn/introducaooo/ContaCorrente.java
@@ -0,0 +1,25 @@
+package br.senac.rn.introducaooo;
+
+
+//vamos herda as caracteristicas do pai;
+//vai usar um extend;
+//quando o extend for feito, o filho(ContaCorrente) vai herda tudo do pai(Conta);
+//chamado tb de herança;
+
+
+public class ContaCorrente extends Conta{
+
+    private Double taxa;
+
+    public void setTaxa(Double taxa) {
+        this.taxa = taxa;
+    }
+
+    public Double getTaxa() {
+        return taxa;
+    }
+
+
+
+}
+
