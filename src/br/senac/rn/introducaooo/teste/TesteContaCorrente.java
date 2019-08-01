7b459a66f39ee4994566ecc0950b3437799644da
MATEUS PERREIRA SATIRO REYES <a9261456@rn.senac.br>
2019-08-01 19:49:41 -0300

    comentarios

--- src/br/senac/rn/introducaooo/teste/TesteContaCorrente.java
+++ src/br/senac/rn/introducaooo/teste/TesteContaCorrente.java
@@ -1,9 +1,18 @@
 package br.senac.rn.introducaooo.teste;
 
+import br.senac.rn.introducaooo.ContaCorrente;
+
 public class TesteContaCorrente {
 
     public static void main(String[] args) {
 
+        ContaCorrente cc = new ContaCorrente();
+
+        cc.setTitular("Flavio Araujo");
+        cc.deposita(800.0);
+        cc.setTaxa(1.0);
+
+        System.out.println(cc);
 
 
     }
